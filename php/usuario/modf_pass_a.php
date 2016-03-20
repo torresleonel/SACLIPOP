fb508b8ad89ddbdf9fa7cbe0f41b29fde8205f00
Leonel Torres <torresleonel18@gmail.com>
2016-03-20 18:53:08 -0430

    separacion modificar usuario (contraseña)
    se completa modificar usuario al crear la segunda opcion de cambiar
    contraseña

--- php/usuario/modf_pass_a.php
+++ php/usuario/modf_pass_a.php
@@ -20,89 +20,31 @@
 			<div id="inicio">
 			</div>
 			<div id="pagina">
-				<?php
-					/*SE LLAMA A LOS ARCHIVOS QUE CONTIENEN LA FUNCION DE CONEXION A LA BD Y LA FUNCION DE CONSULTA USUARIO*/
-					include('../_conexion/conexion_funcion.php');
-					$cnx_bd = conexion();
-					include('../_sql/usuario_sql.php');
-					$resultado = conslt_usuario($cnx_bd);
-					$cnx_bd->close();
-					$fila = $resultado->fetch_object();
-				?>
 				<div id="content">
 					<h1>Modificar Contraseña de Usuario</h1>
 					<div id="wrapper">
 						<div id="navigation">
 							<ul>
 								<li class="selected">
-									<a href="#">Datos</a>
+									<a href="#">Datos del Usuario</a>
 								</li>
 							</ul>
 						</div>
 						<div id="steps">
-							<form id="formElem" name="formElem" action="modf_perfil_b.php" method="post">
+							<form id="formElem" name="formElem" action="modf_pass_b.php" method="post">
 								<fieldset class="step">
-									<legend>Datos del Usuario</legend>
+									<legend>Datos</legend>
 									<div class='derecha'>
-										<div class='campo'>
-											<label for="usuario" class='rotulo' title="Por favor ingrese el nombre de usuario con el que ingresara a SACLIPOP">Usuario</label>
-											<input type="text" name="usuario" value="<?php echo $fila->id_usuario; ?>" id="usuario" title="Por favor ingrese el nombre de usuario con el que ingresara a SACLIPOP" required />
-										</div>
 										<div class='campo'>
 											<label for="clave" class='rotulo' title="Por favor ingrese la nueva clave con la que ingresara a SACLIPOP">Nueva Clave</label>
 											<input type="password" name="clave" id="clave" title="Por favor ingrese la nueva clave con la que ingresara a SACLIPOP" required />
 										</div>
+									</div>
+									<div class='izquierda'>
 										<div class='campo'>
 											<label class='rotulo' for="rep_clave" title="Por favor ingrese nuevamente la clave con la que ingresara a SACLIPOP">Repita Clave</label>
 											<input type="password" name="rep_clave" id="rep_clave" title="Por favor ingrese nuevamente la clave con la que ingresara a SACLIPOP" required />
 										</div>
-									</div>
-									<div class='izquierda'>
-										<div class='campo'>
-											<label for="nombre" class='rotulo' title="Por favor ingrese el nombre del usuario">Nombres</label>
-											<input type="text" name="nombre" value="<?php echo $fila->nombre; ?>" id="nombre" title="Por favor ingrese el nombre del usuario" required />
-										</div>
-										<div class='campo'>
-											<label for="apellido" class='rotulo' title="Por favor ingrese los apellidos del usuario">Apellidos</label>
-											<input type="text" name="apellido" value="<?php echo $fila->apellido; ?>" id="apellido" title="Por favor ingrese los apellidos del usuario" required />
-										</div>
-											<?php if(isset($fila->respuesta)){ ?>
-													<div class='campo'>
-														<div class="select_prgunta">
-															<label class='rotulo_cent' title="Pregunta de seguridad">Pregunta de seguridad</label>
-															<select name="preg_seg" title="Por favor elija una pregunta de seguridad" required>
-																<option value=""></option>
-																<option value="1" <?php if($fila->pregunta == 1) echo 'selected="selected"'; ?>>¿Nombre de su primera mascota?</option>
-																<option value="2" <?php if($fila->pregunta == 2) echo 'selected="selected"'; ?>>¿Nombre de su mejor amigo de la infancia?</option>
-																<option value="3" <?php if($fila->pregunta == 3) echo 'selected="selected"'; ?>>¿Lugar de nacimiento de la madre?</option>
-																<option value="4" <?php if($fila->pregunta == 4) echo 'selected="selected"'; ?>>¿Nombre de su primera escuela?</option>
-																<option value="5" <?php if($fila->pregunta == 5) echo 'selected="selected"'; ?>>¿En que año egreso de la escuela?</option>
-															</select>
-														</div>
-													</div>
-													<div class='campo'>
-														<label for="respuesta" class='rotulo' title="Por favor ingrese la respuesta de seguridad">Respuesta</label>
-														<input type="text" name="respuesta" id="respuesta" title="Por favor ingrese la respuesta de seguridad" required />
-													</div>
-											<?php }else{ ?>
-													<div class='campo'>
-														<div class="select_prgunta">
-															<label class='rotulo_cent' title="Pregunta de seguridad">Pregunta de seguridad</label>
-															<select name="preg_seg" title="Por favor elija una pregunta de seguridad" required>
-																<option value=""></option>
-																<option value="1">¿Nombre de su primera mascota?</option>
-																<option value="2">¿Nombre de su mejor amigo de la infancia?</option>
-																<option value="3">¿Lugar de nacimiento de la madre?</option>
-																<option value="4">¿Nombre de su primera escuela?</option>
-																<option value="5">¿En que año egreso de la escuela?</option>
-															</select>
-														</div>
-													</div>
-													<div class='campo'>
-														<label for="respuesta" class='rotulo' title="Por favor ingrese la respuesta de seguridad">Respuesta</label>
-														<input type="text" name="respuesta" id="respuesta" title="Por favor ingrese la respuesta de seguridad" required />
-													</div>
-											<?php } ?>
 									</div>
 									<div class="bot_cent">
 										<br>
